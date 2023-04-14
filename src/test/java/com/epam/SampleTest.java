a5a7551550b5e6bd1e4925ab9da02b331090f0e1
sairam_pittala <sairam_pittala@epam.com>
2023-04-14 10:35:02 +0530

    Second commit

---
+++ src/test/java/com/epam/SampleTest.java
@@ -0,0 +1,16 @@
+package com.epam;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+    @Test
+    public void test1() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertTrue(true);
+    }
+}
